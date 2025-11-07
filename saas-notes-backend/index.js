daff64b2fc2f66846161166367c1d9fd7075302f
ms-rawat <moharsinghzxz@gmail.com>
2025-11-07 17:08:14 +0530

    fix: update CORS origin to allow requests from production URL; correct casing in App.css import

--- saas-notes-backend/index.js
+++ saas-notes-backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://notes-saas-amber.vercel.app/"],
   credentials: true,               
 }));
 
